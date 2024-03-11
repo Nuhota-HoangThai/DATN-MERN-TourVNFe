5133c6a53b0d0b6f50464d2332e867a97dead7bd
Nuhota-HoangThai <nguyenhoangthai0134@gmail.com>
2024-03-11 10:44:01 +0700

    add tour popular in North

--- src/pages/user/Tours.jsx
+++ src/pages/user/Tours.jsx
@@ -2,6 +2,7 @@ import {} from "react";
 
 import Banner from "../../components/Banner/Banner";
 import PopularCentralRegion from "../../components/PopularCentralRegion/Popular";
+import PopularNorth from "../../components/PopularNorth/PopularNorth";
 import Offers from "../../components/Offers/Offers";
 import NewCollections from "../../components/NewCollections/NewCollections";
 import NewsLetter from "../../components/NewsLetter/NewsLetter";
@@ -11,6 +12,7 @@ const Tours = () => {
     <div className="mt-24">
       <Banner />
       <PopularCentralRegion />
+      <PopularNorth />
       <Offers />
       <NewCollections />
       <NewsLetter />
