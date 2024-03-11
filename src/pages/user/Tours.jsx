784e6ef126745aeba40940e2f0b61a39f41f0456
Nuhota-HoangThai <nguyenhoangthai0134@gmail.com>
2024-03-11 19:39:20 +0700

    import component PopularNorth

--- src/pages/user/Tours.jsx
+++ src/pages/user/Tours.jsx
@@ -2,7 +2,7 @@ import {} from "react";
 
 import Banner from "../../components/Banner/Banner";
 import PopularCentralRegion from "../../components/PopularCentralRegion/Popular";
-//import PopularNorth from "../../components/PopularNorth/PopularNorth";
+import PopularNorth from "../../components/PopularNorth/PopularNorth";
 import Offers from "../../components/Offers/Offers";
 import NewCollections from "../../components/NewCollections/NewCollections";
 import NewsLetter from "../../components/NewsLetter/NewsLetter";
@@ -12,8 +12,8 @@ const Tours = () => {
   return (
     <div className="mt-24">
       <Banner />
+      <PopularNorth />
       <PopularCentralRegion />
-      {/* <PopularNorth /> */}
       <PopularSouthern />
       <Offers />
       <NewCollections />
