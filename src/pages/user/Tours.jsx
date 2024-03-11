060993e0151d14d2164d702c79295e05543ff456
Nuhota-HoangThai <nguyenhoangthai0134@gmail.com>
2024-03-11 12:01:16 +0700

    add southern & north

--- src/pages/user/Tours.jsx
+++ src/pages/user/Tours.jsx
@@ -2,17 +2,19 @@ import {} from "react";
 
 import Banner from "../../components/Banner/Banner";
 import PopularCentralRegion from "../../components/PopularCentralRegion/Popular";
-import PopularNorth from "../../components/PopularNorth/PopularNorth";
+//import PopularNorth from "../../components/PopularNorth/PopularNorth";
 import Offers from "../../components/Offers/Offers";
 import NewCollections from "../../components/NewCollections/NewCollections";
 import NewsLetter from "../../components/NewsLetter/NewsLetter";
+import PopularSouthern from "../../components/PopularSouthern/PopularSouthern";
 
 const Tours = () => {
   return (
     <div className="mt-24">
       <Banner />
       <PopularCentralRegion />
-      <PopularNorth />
+      {/* <PopularNorth /> */}
+      <PopularSouthern />
       <Offers />
       <NewCollections />
       <NewsLetter />
