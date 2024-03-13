96b9f608a9c16b66d3737448aa977de4e0f40e4d
Nuhota-HoangThai <nguyenhoangthai0134@gmail.com>
2024-03-13 10:01:14 +0700

    edit settings

---
+++ src/components/Popular/PopularNorth/PopularNorth.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import Item from "../../Item/Item";
+import { BASE_URL } from "../../../utils/config";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const PopularNorth = () => {
+  const [popularNorthTours, setPopularNorthTours] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/tour/getPopularInNorth`)
+      .then((response) => response.json())
+      .then((data) => setPopularNorthTours(data));
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: popularNorthTours.length >= 3 ? 3 : 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: popularNorthTours.length >= 2 ? 2 : 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="mb-8">
+      <h1 className="text-left pl-24 mb-0.5 text-gray-800 md:text-2xl font-semibold">
+        Tour phổ biến miền Bắc
+      </h1>
+      <div className="border-b-2 mx-24 border-blue-950 mb-5"></div>
+      <div className=" px-24">
+        <div className="">
+          <Slider {...settings}>
+            {popularNorthTours.map((item, i) => (
+              <Item
+                key={i}
+                _id={item._id}
+                image={item.image}
+                nameTour={item.nameTour}
+                price={item.price}
+                regions={item.regions}
+                maxParticipants={item.maxParticipants}
+                startDate={item.startDate}
+                endDate={item.endDate}
+              />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularNorth;
